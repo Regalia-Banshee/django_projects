fff351dd579a0b7ae389f74808f3e1b44ed08639
Regalia-Banshee <siddhurajannair@gmail.com>
2024-05-10 18:58:12 +0530

    Commit for Views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp.views import welcome
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("espanol/", welcome, name="welcome")
+] 
