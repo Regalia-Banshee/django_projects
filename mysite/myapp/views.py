6a8317fec6e7475953b079e240bd2f82ce5d498f
Regalia-Banshee <siddhurajannair@gmail.com>
2024-05-15 16:02:12 +0530

    Third_commit_Templates

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from myapp.models import Profile
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 def welcome(request):
     return render(request,'index.html')
 
@@ -35,4 +37,24 @@ def get_profile_by_name(request, name):
         error_message = f"Profile '{name}' does not exist: {str(e)}"
         error_data = {"error" : error_message}
         return JsonResponse(error_data, status = 404)
-    
+
+def update_email(request,name):
+    if request.method=='PUT':
+        try:
+            profile=Profile.objects.get(name=name)
+            data=json.loads(request.body)
+            new_email=data.get("email")
+            if new_email:
+                profile.email=new_email
+                profile.save()
+                return JsonResponse({"message" : "Email updated successfully"})
+            else:
+                return JsonResponse({"error": "Email not provided in request body"}, status=400)
+
+        except Profile.DoesNotExist as e:
+            error_message = f"Profile '{name}' does not exist: {str(e)}"
+            error_data = {"error" : error_message}
+            return JsonResponse(error_data, status = 404)
+    else:
+        return JsonResponse({"error" : "only 'PUT' request are allowed"}, status = 404)
+
