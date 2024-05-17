05608246dc4e19e9fe287c01d0f81706364a9ea5
Regalia-Banshee <siddhurajannair@gmail.com>
2024-05-17 21:46:20 +0530

    Day_4_Commit

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from myapp.models import Profile
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+import re
 import json
 def welcome(request):
     return render(request,'index.html')
@@ -18,6 +21,7 @@ def ProfileView(request):
         else:
             return render(request, 'profile.html', {"error_message": "Name and Email are required fields"})
         return redirect('profile')
+        
     else:
         stored_profiles = Profile.objects.all()
         context = {'stored_profiles': stored_profiles}
@@ -38,12 +42,21 @@ def get_profile_by_name(request, name):
         error_data = {"error" : error_message}
         return JsonResponse(error_data, status = 404)
 
+
+
+
+  
 def update_email(request,name):
     if request.method=='PUT':
         try:
             profile=Profile.objects.get(name=name)
             data=json.loads(request.body)
             new_email=data.get("email")
+            
+            try:
+                validate_email(new_email)
+            except ValidationError as e:
+                return JsonResponse({"error": str(e)}, status=400)
             if new_email:
                 profile.email=new_email
                 profile.save()
@@ -58,3 +71,30 @@ def update_email(request,name):
     else:
         return JsonResponse({"error" : "only 'PUT' request are allowed"}, status = 404)
 
+def update_name(request,name):
+    if request.method=='PUT':
+        try:
+            profile=Profile.objects.get(name=name)
+            data=json.loads(request.body)
+            new_name=data.get("name")
+            if new_name:
+                profile.name=new_name
+                profile.save()
+                return JsonResponse({"message" : "Name updated successfully"})
+            else:
+                return JsonResponse({"error": "Name not provided in request body"}, status=400)
+
+        except Profile.DoesNotExist as e:
+            error_message = f"Profile '{name}' does not exist: {str(e)}"
+            error_data = {"error" : error_message}
+            return JsonResponse(error_data, status = 404)
+    else:
+        return JsonResponse({"error" : "only 'PUT' request are allowed"}, status = 404)
+
+def Delete_Profile(request,name):
+    if request.method=='POST':
+       Delete_User= Profile.objects.get(name=name) 
+       Delete_User.delete()
+       return JsonResponse({"message":"Profile Deleted Successfully"})
+    else:
+        return JsonResponse({"error" : "only 'POST' request are allowed"}, status = 404)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,10 +20,14 @@ from myapp.views import welcome
 from myapp.views import ProfileView
 from myapp.views import get_profile_by_name
 from myapp.views import update_email
+from myapp.views import update_name
+from myapp.views import Delete_Profile
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("espanol/", welcome, name="welcome"),
     path("name/",ProfileView,name="profile"),
     path("profile/<str:name>/",get_profile_by_name,name="get_profile_by_name"),
     path("profile/<str:name>/email",update_email,name="update_email"),
+    path("profile/<str:name>/update",update_name,name="update_name"),
+    path("profile/<str:name>/delete",Delete_Profile,name="Delete_Profile")
 ] 
